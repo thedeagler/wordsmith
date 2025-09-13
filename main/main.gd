c28dfdc5887614d6dddf7cd4a2da2ccc037b4dd5
Kev <kev@Kevs-Mac-mini.local>
2025-09-13 14:16:22 -0400

    wip chat bubble feature

--- main/main.gd
+++ main/main.gd
@@ -3,4 +3,5 @@ var CONTROLLABLE_CHARACTER_SCENE: String = "res://test/ControllableCharacter2D/T
 var CAMERA_SCENE: String = "res://test/camera/CameraTestScene.tscn"
 
 func _ready():
-	get_tree().change_scene_to_file.call_deferred(CONTROLLABLE_CHARACTER_SCENE)
+	# get_tree().change_scene_to_file.call_deferred(CONTROLLABLE_CHARACTER_SCENE)
+	$SpeechBubble.show_text(Utils.random_symbol_string(14), "legendary")
