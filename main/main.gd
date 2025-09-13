303968a96547dd11b92b53445229c59f76b97bd2
Stephen Wu <stephenwu22@gmail.com>
2025-09-13 00:28:36 -0700

    Move tests into test folder

--- main/main.gd
+++ main/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
-var PLAYER_SCENE: String = "res://player/TestPlayer.tscn"
-var CAMERA_SCENE: String = "res://camera/CameraTestScene.tscn"
+var PLAYER_SCENE: String = "res://test/player/TestPlayer.tscn"
+var CAMERA_SCENE: String = "res://test/camera/CameraTestScene.tscn"
 
 func _ready():
 	get_tree().change_scene_to_file.call_deferred(CAMERA_SCENE)
