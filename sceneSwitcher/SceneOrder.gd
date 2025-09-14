01da74ea435c65467a6aad284416088ae813afdb
Stephen Wu <stephenwu22@gmail.com>
2025-09-14 01:32:27 -0700

    New level backgrounds and town level

--- sceneSwitcher/SceneOrder.gd
+++ sceneSwitcher/SceneOrder.gd
@@ -6,10 +6,14 @@ const CHARACTER_CREATION_SCENE: String = "res://levels/characterCreation/Charact
 const LEVEL1_SCENE: String = "res://levels/Level1.tscn"
 const LEVEL2_SCENE: String = "res://levels/Level2.tscn"
 const LEVEL3_SCENE: String = "res://levels/Level3.tscn"
+const LEVEL4_SCENE: String = "res://levels/Level4.tscn"
+const TOWN_SCENE: String = "res://levels/Town.tscn"
 
 const SCENE_LIST: Array[String] = [
 	CHARACTER_CREATION_SCENE,
 	LEVEL1_SCENE,
 	LEVEL2_SCENE,
-	LEVEL3_SCENE
+	LEVEL3_SCENE,
+	LEVEL4_SCENE,
+	TOWN_SCENE
 ]
