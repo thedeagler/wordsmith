2d7b9c2d36a5d1ef04a9b41a507b982c40b1c936
Stephen Wu <stephenwu22@gmail.com>
2025-09-14 15:23:33 -0700

    Adapt melee to use noun

--- nouns/weapon/melee.gd
+++ nouns/weapon/melee.gd
@@ -5,12 +5,15 @@ class_name MeleeWeapon
 @export var swing_angle: float = 90.0
 @export var damage_amount: int = 50
 
+@export var melee_data: NounData
+
 var swinging: bool = false
-@export var adjectives: Array[AdjectiveData] = []
 
 signal hit(target)
 
 func _ready():
+	# for mvp purposes, set the melee data using the sword_noun.tres
+	melee_data = preload("res://nouns/weapon/sword_noun.tres")
 	$AnimationPlayer.animation_finished.connect(_end_swing)
 	
 func swing(target_position: Vector2):
@@ -45,5 +48,5 @@ func _on_area_entered(area: Area2D) -> void:
 	emit_signal("hit", area)
 
 func add_adjective(adjective: AdjectiveData):
-	adjectives.append(adjective)
-	print("Added adjective '", adjective.word, "' to weapon. Total adjectives: ", adjectives.size())
+	melee_data.adjectives.append(adjective)
+	print("Added adjective '", adjective.word, "' to weapon. Total adjectives: ", melee_data.adjectives.size())
